f4f566e38f3d2565eb28cb03825523ea813f5532
brice <b@bnema.dev>
2026-08-29 17:14:48 +0200

    feat(html): add transactional frame renderer

---
+++ html/benchmark_test.go
@@ -0,0 +1,55 @@
+package html
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/bnema/vev-vt/core"
+)
+
+func BenchmarkRenderer(b *testing.B) {
+	for _, size := range []struct{ width, height int }{{80, 24}, {120, 40}, {240, 80}} {
+		frame := core.NewFrame(size.width, size.height)
+		for y := range size.height {
+			for x := range size.width {
+				frame.Set(x, y, core.Cell{Rune: rune('A' + (x+y)%26), Style: core.DefaultStyle()})
+			}
+		}
+		b.Run(fmt.Sprintf("snapshot-%dx%d", size.width, size.height), func(b *testing.B) {
+			b.ReportAllocs()
+			for range b.N {
+				renderer, _ := New(Options{})
+				prepared, err := renderer.Prepare(frame, nil, true, Cursor{})
+				if err != nil {
+					b.Fatal(err)
+				}
+				if err := prepared.Commit(); err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+		b.Run(fmt.Sprintf("one-row-%dx%d", size.width, size.height), func(b *testing.B) {
+			renderer, _ := New(Options{})
+			first, _ := renderer.Prepare(frame, nil, false, Cursor{})
+			_ = first.Commit()
+			left := frame.Clone()
+			right := frame.Clone()
+			right.Set(0, size.height/2, core.Cell{Rune: 'Z', Style: core.DefaultStyle()})
+			b.ReportAllocs()
+			b.ResetTimer()
+			for index := range b.N {
+				candidate := left
+				if index%2 == 0 {
+					candidate = right
+				}
+				prepared, err := renderer.Prepare(candidate, nil, false, Cursor{})
+				if err != nil {
+					b.Fatal(err)
+				}
+				if err := prepared.Commit(); err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+}
